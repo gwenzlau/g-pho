a6c1401325b3ac31fea36a9cc331a8d1c9e7e071
Grant Wenzlau <grantwenzlau@gmail.com>
2013-06-22 11:26:42 -0700

    bundle up

--- Gemfile
+++ Gemfile
@@ -6,9 +6,12 @@ gem 'rails', '3.2.13'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'pg'
-gem "sqlite3", "~> 1.3.7"
 gem 'paperclip', '~>2.6.0'
 gem 'aws-sdk', '~>1.3.4'
+
+group :development, :test do
+	gem 'sqlite3'
+end
 
 
 # Gems used only for assets and not required
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,18 @@
 class PhotosController < ApplicationController
-	has_attached_file :image,
-                  :styles => { :thumbnail => "100x100#" },
-                  :storage => :s3,
-                  :s3_credentials => S3_CREDENTIALS
+ respond_to :json
+
+  def index
+    @photos = Photo.all
+    respond_with({:photos => @photos}.as_json)
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+    respond_with(@photo)
+  end
+
+  def create
+    @photo = Photo.create(params[:photo])
+    respond_with(@photo)
+  end
 end
