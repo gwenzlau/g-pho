b733cdddf258bc2847ff1f19e2ae1eefdae64185
Grant Wenzlau <grantwenzlau@gmail.com>
2013-06-22 10:47:02 -0700

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GPho::Application.config.secret_token = '12b22a9993f7366f9e7591e903999c28a88ecf9b946508f2841df736c24ab22d21c61e74849d448908c80616d8764965baeecf4c6656a37e39a98bc89f8ae636'
